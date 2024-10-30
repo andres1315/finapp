9958c1affe21b2207d7995927779d6f05d9c6000
Andres Duque <andress1315@gmail.com>
2024-10-29 22:06:24 -0500

    se agrega nuevo sidebar

---
+++ src/dashboard/components/MainSideBar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+
+export function MainSideBar() {
+  return (
+    <Sidebar aria-label="Sidebar with logo branding example" className="">
+      <Sidebar.Logo href="#" img="/finApp.webp" imgAlt="FinApp Logo" className="w-40">
+        Flowbite
+      </Sidebar.Logo>
+      <Sidebar.Items className="min-h-screen  flex items-center">
+        <Sidebar.ItemGroup className="w-full">
+          <Sidebar.Item href="#" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiViewBoards}>
+            Kanban
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiInbox}>
+            Inbox
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiUser}>
+            Users
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiShoppingBag}>
+            Products
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiArrowSmRight}>
+            Sign In
+          </Sidebar.Item>
+          <Sidebar.Item href="#" icon={HiTable}>
+            Sign Up
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
--- src/dashboard/page/DashboardPage.tsx
+++ src/dashboard/page/DashboardPage.tsx
@@ -1,9 +1,10 @@
+import { MainSideBar } from "../components/MainSideBar"
+
 export const DashboardPage = () => {
   return (
     <>
-    <div className="bg-slate-800 h-screen flex flex-col justify-center items-center">
-      <h6 className="text-white font-bold items-center text-4xl">En construccion</h6>
-    </div>
+    <MainSideBar />
+
     </>
   )
 }
